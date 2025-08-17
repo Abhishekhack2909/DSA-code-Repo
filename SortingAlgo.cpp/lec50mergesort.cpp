d20b1ac6bedc994c35d856001a8095cf9f619994
Abhishekhack2909 <abhishektripathi317123@gmail.com>
2025-08-17 21:54:00 +0530

    hi

--- SortingAlgo.cpp/lec50mergesort.cpp
+++ SortingAlgo.cpp/lec50mergesort.cpp
@@ -1,60 +1,62 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 using namespace std;
-void merge(vector<int> &arr, int st,int mid,  int end ){
-    vector<int>temp;
-    int i =st;   
-    int j=mid+1;
-    while(i<=mid && j<=end){
-        if( arr[i]<=arr[j]){
+void merge(vector<int> &arr, int st, int mid, int end)
+{
+    vector<int> temp;
+    int i = st;
+    int j = mid + 1;
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] <= arr[j])
+        {
             temp.push_back(arr[i]);
             i++;
-
-        }else{
+        }
+        else
+        {
             temp.push_back(arr[j]);
             j++;
         }
     }
-        while(i<=mid){// left half 
-            temp.push_back(arr[i]);
-            i++;
-
-
-        }
-        while(j<=end){// right half
-             temp.push_back(arr[j]);
-            j++;
-
-        }
-        for(int idx= 0;idx<temp.size(); idx++){
-            arr[idx+st]=temp[idx];
-
-        } 
-}   
-
- void mergesort( vector<int> &arr, int st, int end){
-    if (st<end){
-        int mid = st+(end-st)/2;
-        mergesort(arr, st,  mid); // for left half 
-
-        mergesort(arr, mid+1, end );// for right half
-        
-        merge( arr, st ,mid,  end);
-
+    while (i <= mid)
+    { // left half
+        temp.push_back(arr[i]);
+        i++;
     }
-
- }
-
-
- int main(){
-    vector<int>arr= {45,23,67,26,12,34};
-    mergesort(arr, 0, arr.size()-1);
-    for( int val:arr){
-        cout<<val<<" ";
+    while (j <= end)
+    { // right half
+        temp.push_back(arr[j]);
+        j++;
     }
-    cout<<endl;
-    return 0;
-  
+    for (int idx = 0; idx < temp.size(); idx++)
+    {
+        arr[idx + st] = temp[idx];
+    }
 }
 
+void mergesort(vector<int> &arr, int st, int end)
+{
+    if (st < end)
+    {
+        int mid = st + (end - st) / 2;
+        // now by recursion
+        mergesort(arr, st, mid); // for left half
 
+        mergesort(arr, mid + 1, end); // for right half
+
+        merge(arr, st, mid, end);
+    }
+}
+
+int main()
+{
+    vector<int> arr = {45, 23, 67, 26, 12, 34};
+    mergesort(arr, 0, arr.size() - 1);
+    for (int val : arr)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+    return 0;
+}
