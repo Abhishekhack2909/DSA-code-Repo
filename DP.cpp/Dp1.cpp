887a6425429d0f88d20e0ac6bcca8c8166b9fcb5
Abhishekhack2909 <abhishektripathi317123@gmail.com>
2025-10-02 22:53:34 +0530

    space optimzation in dp

--- DP.cpp/Dp1.cpp
+++ DP.cpp/Dp1.cpp
@@ -139,7 +139,40 @@ int solveusingmemo(string &text1, string &text2, int i , int j,vector<vector<int
 
 
 
-      }
+        }
+
+        
+
+
+   //  space  optamization in tabulation methods , this will optimize the space based 
+        int solveusingtabulationSO( string text1, string text2){
+        //vector<vector<int>>dp(text1.length()+1, vector<int>(text2.length()+1, 0));
+        vector<int>curr(a.length()+1, 0);
+        vector<int>next(a.length()+1, 0);
+
+
+         for( int i_index=text1.length()-1; i_index>=0; i_index--){
+            for( int j_index=text2.length()-1;j_index>=0; j_index--){
+
+          //recusive logic 
+           int as =0;
+        if(text1[i_index]==text2[j_index]){
+             as=1+next[i_index+1];
+        }
+        else{
+            as= 0 + max(next[i_index],curr[i_index+1]);// this because when the element not matching, then we take one static and other in iterativ and than match , same in revere , and than we take the max of  both of them
+
+        }
+        curr[i_index]=as;
+
+                
+            }
+         }
+         return next[0];
+        }
+
+
+      
     
  
     int longestCommonSubsequence(string text1, string text2) {
